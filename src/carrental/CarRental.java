e596fea0ef51826bfb7333662a74d8ebbac6e5b7
ElyesECE <elyes.benhamida@edu.ece.fr>
2020-11-13 15:04:24 +0100

    try catch

--- src/carrental/CarRental.java
+++ src/carrental/CarRental.java
@@ -5,6 +5,10 @@
  */
 package carrental;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author elyes
@@ -17,6 +21,27 @@ public class CarRental {
     public static void main(String[] args) {
         // TODO code application logic here
         //COUCOU
+        String dBURL = "jdbc:mysql:";
+        String userName = "root";
+        String passWord = "";
+
+        try {
+            
+            Connection conn = DriverManager.getConnection(dBURL, userName, passWord);
+            //Statement stmt = conn.createStatement();
+            //String query = "select IDKey from tTest where nom = 'elyes';";
+            //ResultSet result = stmt.executeQuery(query);
+
+            System.out.println("Connexion ");
+
+        } catch (SQLException ex) {
+
+            System.out.println("Pas de connexion");
+            ex.printStackTrace();
+            System.exit(0);
+
+        };
+
     }
-    
+
 }
