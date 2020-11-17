3a19608721490ce81b1f74a7f3d95d8b1da5afd0
ElyesECE <elyes.benhamida@edu.ece.fr>
2020-11-17 19:27:48 +0100

    Update Registers.java

--- src/carrental/Registers.java
+++ src/carrental/Registers.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package carrental;
+import java.util.ArrayList;
 
 /**
  *
  * @author elyes
  */
 public class Registers {
+    
+    private ArrayList<Order> orderList;
+    
+    Registers(){
+        
+        orderList = new ArrayList<>();
+    }
+    
+    public void addOrder(Order newOrder){
+        orderList.add(newOrder);
+    }
+        
+    
+    
     
 }
