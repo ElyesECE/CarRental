ed40c3ca9a96ca10beac4b4f62f2b34c46b19a25
ElyesECE <elyes.benhamida@edu.ece.fr>
2020-11-18 19:35:35 +0100

    debut du cars

---
+++ src/carrental/Cars.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrental;
+import java.awt.image.BufferedImage;
+/**
+ *
+ * @author elyes
+ */
+public class Cars {
+    
+    //private String ID;
+    private String name;
+    private int range;
+    private int dimension;
+    private boolean gearbox;
+    private double price_per_day;
+    private int max_location_time;
+    private BufferedImage picture;
+    private String consumption;
+    private int seat;
+    private double popularity;
+    
+   
+    
+    
+    
+    
+    
+}
