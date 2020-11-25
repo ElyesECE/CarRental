74251d7a618275bd8ea799bedc8b3338b11fb4c4
ClementRozycki <clement.rozycki@edu.ece.fr>
2020-11-25 17:36:53 +0100

    f

--- src/Model/DatabaseConnector.java
+++ src/Model/DatabaseConnector.java
@@ -19,7 +19,7 @@ public class DatabaseConnector {
 
         String dBURL = "jdbc:mysql://localhost:3306/CarRental?useSSL=false";
         String userName = "root";
-        String passWord = "Elyesbenhamida";
+        String passWord = "axq3fyi9";
         
         
         /*String dBURL = "jdbc:mysql://localhost:3306/CarRental?useSSL=false";
