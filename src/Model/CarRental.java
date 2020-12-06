f61d213a960ab6888083be360f3d20ae54368175
Pedro911-maker <59602284+Pedro911-maker@users.noreply.github.com>
2020-12-07 00:13:51 +0100

    Icons

--- src/Model/CarRental.java
+++ src/Model/CarRental.java
@@ -23,8 +23,8 @@ public class CarRental {
     public static void main(String[] args) {
         // TODO code application logic here
         //COUCOU
-     InitializationDB nouv = new InitializationDB();
-     nouv.InitDB();
+     //InitializationDB nouv = new InitializationDB();
+     //nouv.InitDB();
       
        
        
